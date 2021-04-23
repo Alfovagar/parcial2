ac96c0a10d309e9dd3244a4619f914bc22b3ecf9
Alvaro Folgar <alvarofolgarosorio@gmail.com>
2021-04-22 22:46:04 -0600

    actualizado

--- ConexionBD.h
+++ ConexionBD.h
@@ -8,7 +8,7 @@ private:  MYSQL* conectar;
 public:
 	void abrir_conexion() {
 		conectar = mysql_init(0);
-		conectar = mysql_real_connect(conectar, "localhost", "lesly", "leslyquevedo0016", "crud", 3306, NULL, 0);
+		conectar = mysql_real_connect(conectar, "localhost", "alvaro", "alvarofolgar0016", "crud", 3306, NULL, 0);
 	}
 	MYSQL* getConectar() {
 		return conectar;
